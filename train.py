98dd208b5dec69d8068c48ffed8fc375471bcac7
HATON-R <rhaton@rst-maverick.int-evry.fr>
2025-02-17 13:54:10 +0100

    add train

---
+++ train.py
@@ -0,0 +1,120 @@
+import argparse
+import json
+import logging
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import torch
+import torch.optim
+import models
+import optimizers.regularizers as regularizers
+#from datasets.kg_dataset import KGDataset
+from models import all_models
+from optimizers.kg_optimizer import KGOptimizer
+from utils.train import get_savedir, avg_both, format_metrics, count_params
+from KG_sets import KGDataset
+DATA_PATH = './data'
+
+def train(args):
+    save_dir = get_savedir(args.model, args.dataset)
+
+    # file logger
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename=os.path.join(save_dir, "train.log")
+    )
+
+    # stdout logger
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s %(levelname)-8s %(message)s")
+    console.setFormatter(formatter)
+    logging.getLogger("").addHandler(console)
+    logging.info("Saving logs in: {}".format(save_dir))
+
+    # create dataset
+    #dataset_path = os.path.join(DATA_PATH, args.dataset)
+    #print(dataset_path)
+    dataset_path = "/home/rhaton/test/MetaMobility/knowledge_graph/newyork/Data processed/NYC"
+    #print(dataset_path)
+    dataset = KGDataset(dataset_path, args.debug)
+    args.sizes = dataset.get_shape()
+
+    # load data
+    logging.info("\t " + str(dataset.get_shape()))
+    train_examples = dataset.get_examples("train")
+    valid_examples = dataset.get_examples("valid")
+    test_examples = dataset.get_examples("test")
+    filters = dataset.get_filters()
+
+    # save config
+    with open(os.path.join(save_dir, "config.json"), "w") as fjson:
+        json.dump(vars(args), fjson)
+
+    # create model
+    model = getattr(models, args.model)(args)
+    total = count_params(model)
+    logging.info("Total number of parameters {}".format(total))
+    device = "cpu"
+    model.to(device)
+
+    # get optimizer
+    regularizer = getattr(regularizers, args.regularizer)(args.reg)
+    optim_method = getattr(torch.optim, args.optimizer)(model.parameters(), lr=args.learning_rate)
+    optimizer = KGOptimizer(model, regularizer, optim_method, args.batch_size, args.neg_sample_size,
+                            bool(args.double_neg))
+    counter = 0
+    best_mrr = None
+    best_epoch = None
+    logging.info("\t Start training")
+    for step in range(args.max_epochs):
+
+        # Train step
+        model.train()
+        train_loss = optimizer.epoch(train_examples)
+        logging.info("\t Epoch {} | average train loss: {:.4f}".format(step, train_loss))
+
+        # Valid step
+        model.eval()
+        valid_loss = optimizer.calculate_valid_loss(valid_examples)
+        logging.info("\t Epoch {} | average valid loss: {:.4f}".format(step, valid_loss))
+
+        if (step + 1) % args.valid == 0:
+            valid_metrics = avg_both(*model.compute_metrics(valid_examples, filters))
+            logging.info(format_metrics(valid_metrics, split="valid"))
+
+            valid_mrr = valid_metrics["MRR"]
+            if not best_mrr or valid_mrr > best_mrr:
+                best_mrr = valid_mrr
+                counter = 0
+                best_epoch = step
+                logging.info("\t Saving model at epoch {} in {}".format(step, save_dir))
+                torch.save(model.cpu().state_dict(), os.path.join(save_dir, "model.pt"))
+                model.cpu()
+                #model.cuda()
+            else:
+                counter += 1
+                if counter == args.patience:
+                    logging.info("\t Early stopping")
+                    break
+                elif counter == args.patience // 2:
+                    pass
+
+    logging.info("\t Optimization finished")
+    if not best_mrr:
+        torch.save(model.cpu().state_dict(), os.path.join(save_dir, "model.pt"))
+    else:
+        logging.info("\t Loading best model saved at epoch {}".format(best_epoch))
+        model.load_state_dict(torch.load(os.path.join(save_dir, "model.pt")))
+    model.cpu()
+    #model.cuda()
+    model.eval()
+
+    # Validation metrics
+    valid_metrics = avg_both(*model.compute_metrics(valid_examples, filters))
+    logging.info(format_metrics(valid_metrics, split="valid"))
+
+    # Test metrics
+    test_metrics = avg_both(*model.compute_metrics(test_examples, filters))
+    logging.info(format_metrics(test_metrics, split="test"))
